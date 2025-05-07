1471b0f58ec01c6b732c2b559d53d43698ac5bd1
Michael Campos <mjcampos@users.noreply.github.com>
2025-05-07 15:26:27 +0200

    Game Timer setup
    This will be connected to the slider's status by the game manager singleton

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,34 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class GameTimer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI countdownText;
+    [SerializeField] float totalTime = 20f;
+    
+    float _timeRemaining;
+    int _lastDisplayedSecond;
+    
+    void Start()
+    {
+        _timeRemaining = totalTime;
+        _lastDisplayedSecond = Mathf.CeilToInt(_timeRemaining);
+        countdownText.text = _lastDisplayedSecond.ToString();
+    }
+
+    void Update()
+    {
+        if (_timeRemaining > 0)
+        {
+            _timeRemaining -= Time.deltaTime;
+            int currentSecond = Mathf.CeilToInt(_timeRemaining);
+
+            if (currentSecond < _lastDisplayedSecond)
+            {
+                _lastDisplayedSecond = currentSecond;
+                countdownText.text = _lastDisplayedSecond.ToString();
+            }
+        }
+    }
+}
